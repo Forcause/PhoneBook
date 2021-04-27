6f238b134e05bf93f7575297d4152748068e7ccb
Forcause <hrishtakyan.yum@edu.spbstu.ru>
2021-04-27 22:30:48 +0300

    start creating visual design

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace practice6WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
