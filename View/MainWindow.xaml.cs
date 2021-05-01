0858095b05f1629d0572f68d4c39031216e9c01a
Forcause <hrishtakyan.yum@edu.spbstu.ru>
2021-05-02 01:37:47 +0300

    Add interface and realize MVVM pattern with interfaces, model, view and viewmodel

---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using practice6WPF.ViewModel;
+
+namespace practice6WPF.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new PhoneBookViewModel(new DialogSerivice(), new JsonFileService(), new XMLFileService());
+        }
+    }
+}
